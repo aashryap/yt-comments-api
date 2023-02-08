fe28b0295b150add18c9cb5a6c9ff86e199a69c8
aashryap <aashryap@gmail.com>
2023-02-09 00:22:15 +0530

    cors changes

--- server.js
+++ server.js
@@ -18,12 +18,31 @@ app.use(
   })
 );
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "https://yt-comments-ui.vercel.app",
-    optionSuccessStatus: 200,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://yt-comments-ui.vercel.app",
+//     optionSuccessStatus: 200,
+//   })
+// );
+
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://yt-comments-ui.vercel.app",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
 routes.render(app, router);
 
